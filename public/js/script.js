ef5004ead6cf990f8538eaa79b72709246c519d6
Devill Don <53294521+devillD@users.noreply.github.com>
2022-02-19 17:57:20 +0600

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -8,7 +8,7 @@ async function get_torrent() {
 
   const query = document.getElementById("query").value;
   if(query !== ''){
-    const api = "https://torrenter-api.herokuapp.com//api/" +siteName +"/" +query;
+    const api = "https://api.abirxo.cf/" +siteName +"/" +query;
     
     const html = await axios.get(api);
     const torrents = await html.data;
